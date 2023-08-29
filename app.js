b6c9fef5b81cd6fae28dea465c900bf164db8df2
dogfrontcode <141188652+dogfrontcode@users.noreply.github.com>
2023-08-29 14:28:26 +0000

    enviando o app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+const React = require('react');
+const Component = React.Component;
+const blessed = require('blessed');
+const { render } = require('react-blessed');
+
+
+// Rendering a simple centered box
+class App extends Component {
+  render() {
+    return (
+      <box top="center" left="center" width="50%" height="50%" border={{type: 'line'}} style={{border: {fg: 'cyan'}}}>
+        Hello, React Blessed!
+      </box>
+    );
+  }
+}
+
+// Creating our screen
+const screen = blessed.screen({
+  autoPadding: true,
+  smartCSR: true,
+  title: 'react-blessed hello world'
+});
+
+// Adding a way to quit the program
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+  return process.exit(0);
+});
+
+// Rendering the React app using our screen
+const component = render(<App />, screen);
