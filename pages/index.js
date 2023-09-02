e4df97cba8070cffba8818bf3212192b387acb45
dogfrontcode <141188652+dogfrontcode@users.noreply.github.com>
2023-09-02 13:48:19 +0000

    novo texto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function home () {
-    return <h1> Te amo sua gostosa, quando sair do trabalho saca 100 reais pra gente fumar maconha </h1>
+    return <h1> Te amo sua gostosa </h1>
 }
 
 export default home;
